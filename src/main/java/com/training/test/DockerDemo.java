97c610015d42f88ebfbe7e8301f7c43c90e888fe
parmuthu <parmuthu2002@gmail.com>
2024-04-29 14:56:09 +0530

    initial commit

---
+++ src/main/java/com/training/test/DockerDemo.java
@@ -0,0 +1,10 @@
+package com.training.test;
+
+public class DockerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
